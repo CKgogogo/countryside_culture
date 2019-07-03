bda77e6dbe06d3dfad9df73020f733aee3824587
CK <1327913949@qq.com>
2019-07-03 15:28:13 +0800

    start

--- src/main/java/com/countryside_culture/controller/HomeController.java
+++ src/main/java/com/countryside_culture/controller/HomeController.java
@@ -6,10 +6,12 @@ import com.countryside_culture.service.museumService;
 import com.zlzkj.core.util.Fn;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletResponse;
+import java.text.AttributedString;
 import java.util.List;
 
 @Controller
@@ -19,8 +21,9 @@ public class HomeController {
     private museumService museumservice;
 
     @RequestMapping(value = "/museum",method = RequestMethod.POST)
-    public void showMuseum(HttpServletResponse response){
-        List<museum> museum= museumservice.show();
-        Fn.ajaxReturn(response,museum);
+    public String showMuseum(HttpServletResponse response, Model model) {
+        List<museum> museum = museumservice.show();
+        model.addAttribute("museum", museum);
+        return "index";
     }
 }
